4b62deac0faa481afba907a99947b1f8e67d43e5
Suman Humagain <returnzerosh@gmail.com>
2024-11-19 15:23:11 -0500

    validated user input

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -8,3 +8,19 @@ Random random = new Random();
 int randomInt = random.Next(1, 101);
 
 List<Guess> guessList = new List<Guess>();
+
+int userGuess = 0; // variable to hold the users guess
+
+do
+{
+    Console.Write("Enter your guess: ");
+    string userInput = Console.ReadLine();
+
+    if (!int.TryParse(userInput, out userGuess))
+    {
+        Console.WriteLine("Invalid input. Please enter a valid integer.");
+        continue;
+    }
+
+}
+while (userGuess != randomInt);
