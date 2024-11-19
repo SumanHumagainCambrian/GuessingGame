2f1d3b2ecdcd264ba03fcedca6c938217a1c11f5
Suman Humagain <returnzerosh@gmail.com>
2024-11-19 15:14:27 -0500

    Implemented logic to generate random int

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using GuessingGame;
+
 Console.WriteLine("Hello, World!");
+
+
+Random random = new Random();
+int randomInt = random.Next(1, 101);
+
+List<Guess> guessList = new List<Guess>();
